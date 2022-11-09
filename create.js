14dce887fe1fae674053c37f74bcc58a6c7e3b90
Jummpypants <Jumpypants10@gmail.com>
2022-11-09 18:16:30 +0200

    added random names

--- create.js
+++ create.js
@@ -2,6 +2,55 @@ function randomBetween(min, max){
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function randomName(){
+  var one = ["e","a","i","o","u","ea","ie","ee", "oo", "ae", "ou"];
+  var two = ["qu","t","p","d","g","k","x","c","v","b","h","ch", "ck","w","r","y","s","f","l","z","n","m"];
+
+  function random(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function randomL(l){
+    return l[Math.floor(Math.random() * l.length)];
+  }
+
+  var numNames = 1000;
+
+  var name = "";
+
+  var numLetters = random(3,5);
+
+  var startOne = random(1,2);
+
+  if(startOne == 1){
+    name += randomL(one);
+    numLetters -= 1;
+  }
+
+  for(var i = 0; i < numLetters; i++){
+    var l = random(1,2);
+
+    if(l == 1){
+      name += randomL(two);
+      name += randomL(one);
+      i++;
+    } else if(l == 2 && i > 0){
+      name += randomL(two);
+      name += randomL(two);
+      name += randomL(one);
+      i+=2
+    } else {
+      i--;
+    }
+  }
+
+  capName = name.substr(0, 1);
+  capNameOne = capName.toUpperCase();
+  name = name.replace(capName, capNameOne);
+
+  return(name);
+}
+
 function randomizeEnemies(){
   var numEnemies = randomBetween(constants.enemyMin, constants.enemyMax);
   var level = Math.floor(game.room / constants.enemyRoomsPerlevel);
@@ -16,7 +65,7 @@ function randomizeEnemies(){
 function createHeroes(){
   for(var i = 0; i < constants.startNumHeroes; i++){
     heroes.push({
-      name: names[Math.floor(Math.random() * names.length)],
+      name: randomName(),
       level: 0,
       levelsUsed: 0,
       stats: [{
